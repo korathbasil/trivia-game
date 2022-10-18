005ffbae42e9f6d421e885e4fe7dea3bb6d68ade
Bazil Korath <korathbasil@live.com>
2022-10-18 17:26:55 +0530

    fix : style

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,7 +1,24 @@
-export const Header = () => {
+import { FC, useEffect } from "react";
+
+import styles from "./header.module.scss";
+
+interface HeaderProps {
+  started: boolean;
+}
+
+export const Header: FC<HeaderProps> = ({ started }) => {
+  useEffect(() => {
+    console.log("Rerender");
+  }, []);
   return (
-    <header>
-      <h1>Trivia Game</h1>
+    <header className={styles.header}>
+      <h1
+        style={{
+          textAlign: started ? "left" : "center",
+        }}
+      >
+        Trivia Game
+      </h1>
     </header>
   );
 };
