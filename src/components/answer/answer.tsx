0bc5f51f581e70a94640e5c8b9a6f1e66bb217ed
Bazil Korath <korathbasil@live.com>
2022-10-18 18:58:26 +0530

    feat : add verifying the answers

--- src/components/answer/answer.tsx
+++ src/components/answer/answer.tsx
@@ -1,25 +1,60 @@
-import { FC, FormEvent } from "react";
+import { FC, FormEvent, useState } from "react";
 import styles from "./answer.module.scss";
 
 interface AnswerPops {
-  answer: string;
+  correctAnswer: string;
   questionCount: number;
+  handleNext: () => void;
 }
 
-export const Answer: FC<AnswerPops> = ({ answer, questionCount }) => {
+export const Answer: FC<AnswerPops> = ({
+  correctAnswer,
+  questionCount,
+  handleNext,
+}) => {
+  const [answer, setAnswer] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
-    console.log("Submitted");
+    setSubmitted(true);
   }
   return (
     <form onSubmit={handleSubmit} className={styles.form}>
       <label htmlFor="ans">Please enter your answer below.</label>
-      <input type="text" name="ans" id="ans" />
+      <input
+        type="text"
+        name="ans"
+        id="ans"
+        onChange={(e) => setAnswer(e.target.value)}
+        value={answer}
+        disabled={submitted}
+      />
       <div className={styles.actions}>
-        <button type="submit">Submit</button>
-        <div>
-          <p>Next</p>
+        <button disabled={!answer || submitted} type="submit">
+          Submit
+        </button>
+        <div
+          onClick={() => {
+            handleNext();
+          }}
+        >
+          <p>{submitted ? "Next" : "Skip"}</p>
         </div>
+        {submitted && (
+          <h3
+            style={{
+              color:
+                answer.toLowerCase() === correctAnswer.toLowerCase()
+                  ? "var(--clr-light-green)"
+                  : "var(--clr-danger)",
+            }}
+          >
+            {answer.toLowerCase() === correctAnswer.toLowerCase()
+              ? `Correct Answer +10 points`
+              : "Incorrect Answer"}{" "}
+          </h3>
+        )}
       </div>
     </form>
   );
