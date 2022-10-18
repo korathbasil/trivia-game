8e5df43845af131df4eb7ac0757b883673e8a5ab
Bazil Korath <korathbasil@live.com>
2022-10-18 17:53:51 +0530

    feat : update menu with more options

--- src/components/menu/menu.tsx
+++ src/components/menu/menu.tsx
@@ -1,13 +1,18 @@
 import { FC } from "react";
 
+import styles from "./menu.module.scss";
+
 interface MenuProps {
   startHandler: () => void;
+  highScore: number;
 }
 
-export const Menu: FC<MenuProps> = ({ startHandler }) => {
+export const Menu: FC<MenuProps> = ({ startHandler, highScore }) => {
   return (
-    <section>
+    <section className={styles.menu}>
       <h2 onClick={startHandler}>START</h2>
+      <h2>TOP SCORES</h2>
+      <h2>ABOUT</h2>
     </section>
   );
 };
